8a93cf2170559277f2b91b1c1635195853f390cb
Lincington <lincington@163.com>
2025-09-09 20:25:22 +0800

    jkkl

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -7,12 +7,20 @@ namespace Lottery
     {     
         static async Task Main(string[] args)
         {
-
-           string json =  File.ReadAllText("A.txt").Replace("\r\n","\t");
-
+            string json =  File.ReadAllText("A.txt").Replace("\r\n","\t");
             string[] jkl = json.Trim().Split('\t');
+            while (true)
+            {
+                Random random = new Random();
+                // 生成包含6个随机数字字符串的数组
+                string[] randomStrings = new string[6];
 
-            Console.ReadLine();
+                for (int i = 0; i < 6; i++)
+                {
+                    randomStrings[i] = jkl[random.Next(0, jkl.Length - 1)]; // 0-9的数字
+                }
+                File.AppendAllText("B.csv", string.Join(",", randomStrings) + "\r\n");
+            }
         }
     }
 }
