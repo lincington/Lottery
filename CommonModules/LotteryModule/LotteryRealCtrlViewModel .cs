bbefdecc6bd94b059f1c120a44abc69599384473
jiangliangdun <jiangliangdun@outlook.com>
2025-09-14 22:05:00 +0800

    data

--- CommonModules/LotteryModule/LotteryRealCtrlViewModel .cs
+++ CommonModules/LotteryModule/LotteryRealCtrlViewModel .cs
@@ -19,29 +19,37 @@ namespace CommonModules.LotteryModule
 
         DispatcherTimer readDataTimer = new  DispatcherTimer();
   
-        List<double> dataX = new List<double> () ;
-        List<double> dataY = new List<double>();
-        List<double> dataYY = new List<double>();
+
         public LotteryRealCtrlViewModel()
         {
             _red = new WpfPlot();
-            for (int i = 1; i < 34; i++)
-            {
-                dataX.Add(i);
-                dataY.Add(0);
-                if (i < 17)
-                {
-                    dataYY.Add(0);
-                }
-            }     
+           
             readDataTimer.Tick += ReadDataTimer_Tick;
-            readDataTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            readDataTimer.Interval = new TimeSpan(0, 0, 0, 1,300);
             readDataTimer.Start();
         }
 
         private void ReadDataTimer_Tick(object? sender, EventArgs e)
         {
-            IEnumerable<Lottery> collection =   DoubleColorBallGenerator.GenerateSQLTickets(33*16).ToList();
+            List<double> dataX = new List<double>();
+            List<double> dataY = new List<double>();
+            List<double> dataYY = new List<double>();
+
+            List<double> dataAVY = new List<double>();
+            List<double> dataAVYY = new List<double>();
+            for (int i = 1; i < 34; i++)
+            {
+                dataX.Add(i);
+                dataY.Add(0);
+                dataAVY.Add(0);
+                if (i < 17)
+                {
+                    dataYY.Add(0);
+                    dataAVYY.Add(0);
+                }
+            }
+
+            IEnumerable<Lottery> collection =   DoubleColorBallGenerator.GenerateSQLTickets(3352).ToList();
 
             foreach (var item in collection)
             {
@@ -53,9 +61,22 @@ namespace CommonModules.LotteryModule
                 dataY[item.R6 - 1]++;
                 dataYY[item.B1 - 1]++;
             }
+
+            for (int i = 0; i < 33; i++)
+            {
+                dataAVY[i]= (dataY.Sum()/33);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                dataAVYY[i] = (dataYY.Sum() / 16);
+            }
             Red.Plot.Clear();
             Red.Plot.Add.Scatter(dataX.ToArray(), dataY.ToArray());
             Red.Plot.Add.Scatter(dataX.ToArray(), dataYY.ToArray());
+            Red.Plot.Add.Scatter(dataX.ToArray(), dataAVY.ToArray());
+
+            Red.Plot.Add.Scatter(dataX.ToArray(), dataAVYY.ToArray());
             Red.Plot.Axes.AutoScale();
             Red.Refresh();
         }
