634e67b67f0a7f15cba0ee40e43981b1be9bd439
Lincington <lincington@163.com>
2026-02-12 13:38:54 +0800

    FGFGHFH

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -4,7 +4,7 @@ namespace Lottery
 {
     public class Program
     {     
-        static void Main(string[] args)
+        static void Main(string[]  args)
         {
             #region =======================================================================
             //var services = new ServiceCollection();
